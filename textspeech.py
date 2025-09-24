cfaac01f7e1f76b930de0a64e1b0d6a5e232fde0
erick847 <erick@beem.africa>
2025-09-24 17:21:32 +0300

    Initial UI commit

--- textspeech.py
+++ textspeech.py
@@ -1,25 +1,50 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from wtforms import Form, StringField, validators, SubmitField
 import requests
 import os
 from requests.auth import HTTPBasicAuth as htt
 from dotenv import load_dotenv
 load_dotenv()
 
-username = os.getenv("TELESIGN_ID")
-password = os.getenv("TELESIGN_KEY")
+app = Flask(__name__)
+class InputForm(Form):
+    phone_number = StringField('Phone Number', [validators.DataRequired(), validators.Length(min=10, max=15)])
+    submit = SubmitField('Call Me!')
 
-url = "https://rest-ww.telesign.com/v1/verify/call"
 
-payload = { "language": "en-US", 
-            "verify_code": "1234", 
-            "phone_number": "+255696606112", 
-            "ucid": "BACS" }           
- 
 
-headers = {
-    "accept": "application/json",
-    "content-type": "application/x-www-form-urlencoded"
-}
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-response = requests.post(url, data=payload, headers=headers, auth=htt(username, password))
+@app.route('/call', methods=['GET', 'POST'])
+def call():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        phone_number = form.phone_number.data
 
-print(response.text)
+        username = os.getenv("TELESIGN_ID")
+        password = os.getenv("TELESIGN_KEY")
+
+        url = "https://rest-ww.telesign.com/v1/verify/call"
+
+        payload = { "language": "en-US", 
+                    "verify_code": "1234", 
+                    "phone_number": phone_number, 
+                    "ucid": "BACS" }           
+        
+
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/x-www-form-urlencoded"
+        }
+
+
+        response = requests.post(url, data=payload, headers=headers, auth=htt(username, password))
+
+        print(response.text)
+        return redirect(url_for('home'))
+    return render_template('call.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
