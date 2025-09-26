75681509c7bda9c86f0f8c54658666812e33ce6a
erick847 <erick@beem.africa>
2025-09-26 16:00:29 +0300

    Added functionality to send SMS via Beem API if no code is detected in the message.

--- textspeech.py
+++ textspeech.py
@@ -34,31 +34,62 @@ def call():
                 if word.isdigit() and len(word) < 6:
                     return word 
 
-
 # Get TeleSign credentials from environment variables
-        username = os.getenv("TELESIGN_ID")
-        password = os.getenv("TELESIGN_KEY")
+        username_call = os.getenv("TELESIGN_ID")
+        password_call = os.getenv("TELESIGN_KEY")
+
+        username_sms = os.getenv("SMS_API_KEY")
+        password_sms = os.getenv("SMS_API_SECRET")
 
         telesign_url = "https://rest-ww.telesign.com/v1/verify/call"
+        beem_sms_url = "https://apisms.beem.africa/v1/send"
 
 # Prepare the payload for the API request
-        payload = { "language": "en-US", 
+        payload_call = { "language": "en-US", 
                     "verify_code": strip_code(text_message), 
                     "phone_number": phone_number, 
                     "ucid": "BACS" }           
         
 
-        headers = {
+        headers_call = {
             "accept": "application/json",
             "content-type": "application/x-www-form-urlencoded"
         }
 
-# Make the API request to TeleSign
-        response = requests.post(telesign_url, data=payload, headers=headers, auth=htt(username, password))
+        payload_sms = {
+            "source_addr": "BEEMERS",
+            "schedule_time": "",
+            "encoding": "0",
+            "message": text_message,
+            "recipients": [
+                {
+                    "recipient_id": 1,
+                    "dest_addr": phone_number
+                }
+            ]
+        }
 
-        print(response.text)
+        headers_sms = {
+            "accept": "application/json",
+            "content-type": "application/json"
+        }
+
+# Make the API request to TeleSign to call the user if there is a code in the message, otherwise send SMS via Beem API
+        if strip_code(text_message):
+            response_call = requests.post(telesign_url, data=payload_call, headers=headers_call, auth=htt(username_call, password_call))
+            print(response_call.text)
+        else:
+            response_sms = requests.post(beem_sms_url, json=payload_sms, headers=headers_sms, auth=htt(username_sms, password_sms))
+            print(response_sms.text)      
+        
+
+# Redirect to home after submission
+
         return redirect(url_for('home'))
     return render_template('call.html', form=form)
+
+# modify the function to send SMS if call is not picked or there is no code in the message
+
 
 if __name__ == '__main__':
     app.run(debug=True)
